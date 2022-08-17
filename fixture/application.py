a60b48492fc445f9c014f00f333932bddaca0174
akolchinskij <artem94@list.ru>
2022-08-17 11:11:40 +0300

    Доработки по хелперам и логауту

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session_helper import SessionHelper
-from fixture.group_contact_helper import GroupContactHelper
+from fixture.group_helper import GroupHelper
+from fixture.contact_helper import ContactHelper
 
 
 class Application:
@@ -9,8 +10,8 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-        self.group = GroupContactHelper(self)
-        self.contact = GroupContactHelper(self)
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_main_page(self):
         wd = self.wd
--- fixture/contact_helper.py
+++ fixture/contact_helper.py
@@ -1,60 +1,7 @@
-class GroupContactHelper:
+class ContactHelper:
 
     def __init__(self, app):
         self.app = app
-
-    def create_group(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # create_new_group
-        wd.find_element_by_name("new").click()
-        # complete_form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # save_group
-        wd.find_element_by_name("submit").click()
-        self.open_groups_page()
-
-    def edit_first_group(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # select_first_group
-        wd.find_element_by_name("selected[]").click()
-        # click_button_Edit
-        wd.find_element_by_name("edit").click()
-        # complete_form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # save_edited_group
-        wd.find_element_by_name("update").click()
-        self.open_groups_page()
-
-    def delete_first_group(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # delete selected group
-        wd.find_element_by_name("delete").click()
-        self.open_groups_page()
-
-    def open_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
 
     def create_contact(self, contact):
         wd = self.app.wd
